1dbfa0e1d539060055da859083eecb6e5ef993ee
Vincent Hall <jackvhall@outlook.com>
2020-06-07 08:06:32 -0700

    add staging environment for telescope provider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,7 +21,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment(['local', 'production'])) {
+            if ($this->app->environment(['local', 'staging', 'production'])) {
                 return true;
             }
 
